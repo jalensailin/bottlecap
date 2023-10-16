a034b4c962657c20e4fbec7e4d26a8c8011665cf
jalensailin <jalen.michalslevy@gmail.com>
2023-10-16 19:12:41 -0400

    insert a placeholder button; simple styling;

--- bottlecap.js
+++ bottlecap.js
@@ -1,5 +1,6 @@
-/* global Hooks game randomID */
+/* global Hooks game randomID FormApplication mergeObject */
 
+// eslint-disable-next-line max-classes-per-file
 class BottleCap {
   static ID = "bottlecap";
 
@@ -39,5 +40,22 @@ class BottleCap {
 
 Hooks.once("init", () => {
   game.bottlecap = BottleCap;
+
   console.log(BottleCap.ID);
+});
+
+Hooks.on("renderPlayerList", (playerList) => {
+  // All of this is jquery stuff.
+  const playersHeader = playerList.element.find("h3");
+  const bottleCapNumber = Object.keys(
+    game.user.getFlag(BottleCap.ID, BottleCap.FLAG),
+  ).length;
+
+  const button = `<button type='button' class='flexrow flex0 bottlecap-button'>${bottleCapNumber}<i class='fas fa-tasks'></i></button>`;
+  playersHeader.addClass("flexrow");
+  playersHeader.append(button);
+
+  playersHeader.on("click", "button", (event) => {
+    event.stopPropagation();
+  });
 });
